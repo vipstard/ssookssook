605d5b1675c77771fcb9d361214d88aa1e412615
westty22 <cdh9441@naver.com>
2022-05-02 16:58:10 +0900

    2022-05-22 Login

--- app/src/main/java/com/example/ssukssuk/LoginActivity.java
+++ app/src/main/java/com/example/ssukssuk/LoginActivity.java
@@ -3,8 +3,10 @@ package com.example.ssukssuk;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +14,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-    Button login, newlogin;
+    Button login, basic,idfind,pwfind;
     EditText user_id, user_pw;
 
     @Override
@@ -23,31 +25,65 @@ public class LoginActivity extends AppCompatActivity {
         user_id = findViewById(R.id.user_id);
         user_pw = findViewById(R.id.user_pw);
         login = findViewById(R.id.btn_login);
-        newlogin = findViewById(R.id.button2);
-        newlogin.setOnClickListener(new View.OnClickListener() {
+        basic = findViewById(R.id.btn_basic);
+        idfind = findViewById(R.id.btn_idfind);
+        pwfind = findViewById(R.id.btn_pwfind);
+
+        SharedPreferences auto = getSharedPreferences("auto", Activity.MODE_PRIVATE);
+
+        String id = user_id.getText().toString();
+        String pw = user_pw.getText().toString();
+
+        if(id!=null && pw != null) {
+            if(user_id.equals("sc") && user_pw.equals("sc")) {
+                Toast.makeText(LoginActivity.this, user_id +"님 자동로그인 입니다.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+
+        else if(id == null && pw == null){
+            login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+
+
+                    if (id.equals("sc") && pw.equals("sc")) {
+                        SharedPreferences auto = getSharedPreferences("auto", Activity.MODE_PRIVATE);
+
+                        SharedPreferences.Editor autoLogin = auto.edit();
+                        autoLogin.putString("user_id", id);
+                        autoLogin.putString("user_pw",pw);
+                        //꼭 commit()을 해줘야 값이 저장됩니다 ㅎㅎ
+                        autoLogin.commit();
+                        Toast.makeText(LoginActivity.this, user_id.getText().toString()+"님 환영합니다.", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }else{
+                        Toast.makeText(LoginActivity.this,
+                                "로그인 실패",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        basic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showDialog();
             }
         });
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String id = user_id.getText().toString();
-                String pw = user_pw.getText().toString();
-                if (id.equals("sc") && pw.equals("sc")){
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                }
-                else{
-                    Toast.makeText(LoginActivity.this,
-                            "로그인 실패",
-                            Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
 
-    }
+
+
+
+
+
+
+    }}
     void showDialog() {
         AlertDialog.Builder msgBuilder = new AlertDialog.Builder(LoginActivity.this)
                 .setTitle("회원가입페이지 이동")
@@ -69,5 +105,6 @@ public class LoginActivity extends AppCompatActivity {
         AlertDialog msgDlg = msgBuilder.create();
         msgDlg.show();
     }
+
 
 }
---
+++ app/src/main/java/com/example/ssukssuk/idfindActivity.java
@@ -0,0 +1,14 @@
+package com.example.ssukssuk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class idfindActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_idfind);
+    }
+}
