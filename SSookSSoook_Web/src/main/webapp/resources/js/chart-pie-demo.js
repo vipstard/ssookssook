ec55ac2129ea70be4ca40155281598993106130b
조현빈 <97923969+cho-hyeon-bin@users.noreply.github.com>
2022-05-16 14:33:00 +0900

    메뉴 수정 /  매출게시판 / 유저정보게시판
    추후 수정할게 있지만 일단은 끝

---
+++ SSookSSoook_Web/src/main/webapp/resources/js/chart-pie-demo.js
@@ -0,0 +1,38 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+(Chart.defaults.global.defaultFontFamily = "Nunito"),
+  '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = "#858796";
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: ["활동 회원", "휴면 회원", "탈퇴 회원"],
+    datasets: [
+      {
+        data: [70, 20, 10],
+        backgroundColor: ["#019267", "#5D8BF4", "#C0D8C0"],
+        hoverBackgroundColor: ["#F4BBBB", "#FFB72B", "#A267AC"],
+        hoverBorderColor: "rgba(234, 236, 244, 1)",
+      },
+    ],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: "#dddfeb",
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false,
+    },
+    cutoutPercentage: 80,
+  },
+});
