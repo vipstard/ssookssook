dbd7f4e35077329fd9fbee559f83cb9eade95448
Hongsungcahe <ghdtjdco3@naver.com>
2022-05-19 14:33:04 +0900

    성채 - 일기 내용 맞게 수정

--- app/src/main/java/com/example/ssukssuk/DiaryActivity2.java
+++ app/src/main/java/com/example/ssukssuk/DiaryActivity2.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,8 +15,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.ssukssuk.Board.BoardVO;
+import com.example.ssukssuk.VO.Board_list_select_writeVO;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -24,6 +32,9 @@ public class DiaryActivity2 extends AppCompatActivity {
     TextView dia_title;
     TextView dia_content;
     Button btn_reg;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+     DatabaseReference myRef = database.getReference("Diary");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,7 +62,29 @@ public class DiaryActivity2 extends AppCompatActivity {
         dia_title = findViewById(R.id.diary_title);
         dia_title.setText(title);
         dia_content = findViewById(R.id.diary_content);
-        dia_content.setText(content);
+
+        myRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                } else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                    DataSnapshot snapshot = task.getResult();
+
+                    for (DataSnapshot data : snapshot.getChildren()) {
+                        BoardVO vo = data.getValue(BoardVO.class);
+
+                        if (title.equals(vo.getTitle())) {
+                            String content1 = vo.getContent();
+                            dia_content.setText(content1);
+                        }
+                }
+            }
+        }
+    });
         FirebaseStorage storage = FirebaseStorage.getInstance("gs://ssukssuk-af5d6.appspot.com/");
         StorageReference storageRef = storage.getReference();
         storageRef.child("images/" + date + ".jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
